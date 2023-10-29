e46ede18cf53146560b321e73d5147182930c2d3
José Urbina <joseurbina0208@gmail.com>
2023-10-28 23:59:43 -0300

    implementación de validación completa en registro, además de revisión de barra superior

--- src/componentes/barranavegacion.jsx
+++ src/componentes/barranavegacion.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./barranavegacion.css";
 
 //barra de navegacion
 export const BarraNavegacion = () => {
   return (
-    <div className="navbar">
-      <div className="links">
-        <Link to="/">Inicio</Link>
-        <Link to="/bandejaentrada">be</Link>
-        <Link to="/busqueda">bu</Link>
-        <Link to="/chat">ch</Link>
-        <Link to="/iniciarsesion">in</Link>
-        <Link to="/notificaciones">no</Link>
-        <Link to="/registrar">re</Link>
+    <div className="w-full h-16 bg-white grid grid-cols-3 border">
+      <Link to="/" className="px-4 justify-start col-span-1 my-auto">
+        <hp class="ml-5 w-14 h-10 bg-blue-600 rounded-xl flex items-center justify-center font-bold text-lg text-white drop-shadow-lg">
+          CoPra
+        </hp>
+      </Link>
+      <div className="mr-5 flex items-center font-bold text-blue-600 justify-end col-span-2">
+        <Link to="/bandejaentrada" className="px-4">
+          Bandeja de Entrada
+        </Link>
+        <Link to="/busqueda" className="px-4">
+          Búsqueda
+        </Link>
+        <Link to="/chat" className="px-4">
+          Chat
+        </Link>
+        <Link to="/iniciarsesion" className="px-4">
+          Iniciar Sesión
+        </Link>
+        <Link to="/notificaciones" className="px-4">
+          Notificaciones
+        </Link>
+        <Link to="/registrar" className="px-4">
+          Registrarse
+        </Link>
       </div>
     </div>
   );
--- src/paginas/registrar/registrar.jsx
+++ src/paginas/registrar/registrar.jsx
@@ -1,42 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export const Registrar = () => {
+  // Inicializamos el formulario usando react-hook-form
   const {
-    register,
-    formState: { errors },
-    handleSubmit,
+    register, // Función para registrar campos del formulario
+    handleSubmit, // Función para manejar la presentación del formulario
+    formState: { errors }, // Objeto que contiene los errores del formulario
+    getValues, // Función para obtener los valores del formulario
   } = useForm();
 
+  const [rutErrorMessage, setRutErrorMessage] = useState(""); // Estado para el mensaje de error del RUT
+
   const onSubmit = (data) => {
-    console.log(data);
+    console.log(data); // Función que se ejecutará al enviar el formulario
+  };
+
+  // Función para validar el RUT
+  const validateRut = (value) => {
+    // Función para calcular el dígito verificador del RUT
+    const calculateDV = (rut) => {
+      let M = 0;
+      let S = 1;
+      let T = parseInt(rut);
+
+      for (; T !== 0; T = Math.floor(T / 10)) {
+        S = (S + (T % 10) * (9 - (M++ % 6))) % 11;
+      }
+
+      return S > 0 ? String(S - 1) : "k";
+    };
+
+    // Función para validar el formato del RUT y su dígito verificador
+    const validaRut = (rut) => {
+      const pattern = /^[0-9]+-[0-9kK]{1}$/; // Patrón para el formato del RUT
+      if (!pattern.test(rut)) return false;
+
+      const [numero, dv] = rut.split("-");
+      return dv.toLowerCase() === calculateDV(numero);
+    };
+
+    if (validaRut(value)) {
+      setRutErrorMessage(""); // Si el RUT es válido, se borra el mensaje de error
+      return true;
+    } else {
+      setRutErrorMessage("RUT inválido"); // Si el RUT es inválido, se establece el mensaje de error
+      return false;
+    }
   };
 
   return (
-    <div class="flex justify-center pt-16">
-      <div class="w-1/2">
+    <div className="flex justify-center pt-16">
+      <div className="w-1/2">
         <form
-          class="px-10 bg-gray-50 border rounded-md drop-shadow-lg"
-          onSubmit={handleSubmit(onSubmit)}
+          className="px-10 bg-gray-50 border rounded-md shadow-lg"
+          onSubmit={handleSubmit(onSubmit)} // Manejo de la presentación del formulario
         >
           <div>
-            <h1 class="text-4xl font-bold text-center pt-10">
+            <h1 className="text-4xl font-bold text-center pt-10">
               Crear una cuenta nueva
             </h1>
-            <p class="text-center py-10">
+            <p className="text-center py-10">
               Ingrese sus datos para crear su cuenta nueva, todos los campos son
               obligatorios a menos que se indique lo contrario
             </p>
           </div>
-          <div class="grid grid-cols-8">
-            <div class="col-span-5 flex justify-center p-2">
+          <div className="grid grid-cols-4">
+            <div className="col-span-2 flex flex-col p-2">
+              {/* Campo de Nombre */}
               <input
-                class=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full shadow-lg py-1"
                 type="text"
                 id="username"
-                placeholder="Nombre de usuario"
+                placeholder="Nombre y apellidos"
                 {...register("username", {
-                  required: true,
+                  required: "Este campo es obligatorio",
                   minLength: {
                     value: 2,
                     message: "El nombre debe tener al menos 2 caracteres",
@@ -44,77 +82,75 @@ export const Registrar = () => {
                 })}
               />
               {errors.username && (
-                <strong class="col-span-full flex justify-start p-2">
+                <strong className="text-red-600">
                   {errors.username.message}
                 </strong>
               )}
             </div>
-            <div class="col-span-3 flex justify-center p-2">
+            <div className="col-span-2 flex flex-col p-2">
+              {/* Campo de entrada de RUT */}
               <input
-                class=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full shadow-lg py-1"
                 type="text"
                 id="rut"
                 placeholder="Rut (con guión y sin puntos)"
                 {...register("rut", {
-                  required: true,
-                  minLength: {
-                    value: 9,
-                    message: "Rut debe tener al menos 9 caracteres",
-                  },
+                  required: "Este campo es obligatorio",
+                  validate: (value) => validateRut(value), // Validación personalizada para el RUT
                 })}
               />
+              {errors.rut && (
+                <strong className="text-red-600">{errors.rut.message}</strong>
+              )}
+              {rutErrorMessage && (
+                <strong className="text-red-600">{rutErrorMessage}</strong>
+              )}
             </div>
-            {errors.rut && (
-              <strong class="col-span-full flex justify-center p-2">
-                {errors.rut.message}
-              </strong>
-            )}
-
-            <div class="col-span-4 flex justify-center p-2">
+            <div className="col-span-3 flex flex-col p-2">
+              {/* Campo de Correo Electrónico */}
               <input
-                class=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full shadow-lg py-1"
                 type="email"
                 id="email"
                 placeholder="Correo electrónico"
                 {...register("email", {
-                  required: true,
-                  minLength: {
+                  required: "Este campo es obligatorio",
+                  pattern: {
                     value: /\S+@\S+\.\S+/,
-                    message: "Ingrese un correo electronico valido",
+                    message: "Ingrese un correo electrónico válido",
                   },
                 })}
               />
+              {errors.email && (
+                <strong className="text-red-600">{errors.email.message}</strong>
+              )}
             </div>
-            {errors.email && (
-              <strong class="col-span-full flex justify-center p-2">
-                {errors.email.message}
-              </strong>
-            )}
-            <div class="col-span-4 flex justify-center p-2">
+            <div className="col-span-1 flex flex-col p-2">
+              {/* Campo de Teléfono */}
               <input
-                class=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full shadow-lg py-1"
                 type="tel"
                 id="tel"
-                placeholder="Telefono"
+                placeholder="Teléfono"
                 {...register("tel", {
-                  required: true,
+                  required: "Este campo es obligatorio",
                   minLength: {
                     value: 9,
-                    message: "Telefono debe tener al menos 9 caracteres",
+                    message: "Teléfono debe tener al menos 9 caracteres",
                   },
                 })}
               />
+              {errors.tel && (
+                <strong className="text-red-600">{errors.tel.message}</strong>
+              )}
             </div>
-            {errors.tel && (
-              <strong class="col-span-full flex justify-center p-2">
-                {errors.tel.message}
-              </strong>
-            )}
-
-            <div class="col-span-4 flex justify-center p-2">
+            <div className="col-span-2 flex flex-col p-2">
+              {/* Campo de Región */}
               <select
-                class="bg-white border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
-                required
+                className="bg-white border border-black rounded-md pl-2 w-full shadow-lg py-1"
+                {...register("region", {
+                  required: "Seleccione una región",
+                })}
               >
                 <option value="" disabled selected>
                   Región
@@ -122,12 +158,19 @@ export const Registrar = () => {
                 <option value="test1">Región de ejemplo 1</option>
                 <option value="test2">Región de ejemplo 2</option>
               </select>
+              {errors.region && (
+                <strong className="text-red-600">
+                  {errors.region.message}
+                </strong>
+              )}
             </div>
-
-            <div class="col-span-4 flex justify-center p-2">
+            <div className="col-span-2 flex flex-col p-2">
+              {/* Campo de Comuna */}
               <select
-                class="bg-white border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
-                required
+                className="bg-white border border-black rounded-md pl-2 w-full shadow-lg py-1"
+                {...register("comuna", {
+                  required: "Seleccione una comuna",
+                })}
               >
                 <option value="" disabled selected>
                   Comuna
@@ -135,54 +178,70 @@ export const Registrar = () => {
                 <option value="test3">Comuna de ejemplo 1</option>
                 <option value="test2">Comuna de ejemplo 2</option>
               </select>
+              {errors.comuna && (
+                <strong className="text-red-600">
+                  {errors.comuna.message}
+                </strong>
+              )}
             </div>
-            <div class="col-span-4 flex justify-center p-2">
+            <div className="col-span-2 flex flex-col p-2">
+              {/* Campo de Contraseña */}
               <input
-                class=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full shadow-lg py-1"
                 type="password"
                 id="password"
                 placeholder="Contraseña"
                 {...register("password", {
-                  required: true,
+                  required: "Este campo es obligatorio",
                   minLength: {
                     value: 8,
                     message: "La contraseña debe tener al menos 8 caracteres",
                   },
                 })}
               />
+              {errors.password && (
+                <strong className="text-red-600">
+                  {errors.password.message}
+                </strong>
+              )}
             </div>
-            {errors.password && (
-              <strong class="col-span-full flex justify-center">
-                {errors.password.message}
-              </strong>
-            )}
-
-            <div class="col-span-4 flex justify-center p-2">
+            <div className="col-span-2 flex flex-col p-2">
+              {/* Campo de Confirmación de Contraseña */}
               <input
-                class=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full shadow-lg py-1"
                 type="password"
                 id="passwordConf"
                 placeholder="Confirmar contraseña"
-                {...register("passwordConf", { required: true })}
+                {...register("passwordConf", {
+                  required: "Este campo es obligatorio",
+                  validate: (value) =>
+                    value === getValues("password") ||
+                    "Las contraseñas no coinciden",
+                })}
               />
+              {errors.passwordConf && (
+                <strong className="text-red-600">
+                  {errors.passwordConf.message}
+                </strong>
+              )}
             </div>
-            <div class="col-span-full p-2">
+            <div className="col-span-4 p-2">
+              {/* Checkbox para aceptar términos y condiciones */}
               <input
-                class="drop-shadow-lg w-4 h-4"
+                className="shadow-lg w-4 h-4"
                 type="checkbox"
                 id="terms"
-                placeholder="Términos y condiciones"
                 required
               />
               <label htmlFor="checkbox">
                 Acepto los términos y condiciones
               </label>
             </div>
-
-            <div class="col-span-full py-10 flex justify-center">
+            <div className="col-span-full py-10 flex justify-center">
+              {/* Botón de registro */}
               <button
                 type="submit"
-                class="w-1/3 h-12 bg-blue-600 rounded-md text-white font-bold drop-shadow-lg border border-gray-400"
+                className="w-1/3 h-12 bg-blue-600 rounded-md text-white font-bold shadow-lg border border-gray-400"
               >
                 Registrar
               </button>
