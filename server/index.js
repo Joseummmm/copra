5ac418c06e9afccd5b67b4ae90eae0c2ed996547
Jose Urbina <joseurbina0208@gmail.com>
2023-11-16 21:44:09 -0300

    apirest

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ con.connect(function (error) {
     if (error) {
         console.log("error conectando");
         return;
-    }
-    else {
+    } else {
         console.log("conexion exitosa");
     }
 });
@@ -26,12 +25,180 @@ app.listen(configuracion, () => {
     console.log(`Conectando al servidor http://localhost:${configuracion.port}`);
 });
 
-app.get("", (req, res) => {
-    con.query("select * from practicas", function (error, results, field) {
-        res.send(JSON.stringify(results));
+app.use(bodyParser.json());
+
+app.post('/insertarPracticas', (req, res) => {
+    const practicas = req.body.practicas;
+  
+    // Iterar sobre las prácticas y realizar la inserción en la base de datos
+    practicas.forEach((practica) => {
+      const { id, title, nombreEmpresa, ubicacion, descripcion, category, imagen } = practica;
+  
+      const query = 'INSERT INTO practicas (id_practica, nombre_practica, datos_practica) VALUES (?, ?, ?)';
+      const values = [id, title, JSON.stringify({ nombreEmpresa, ubicacion, descripcion, category, imagen })];
+  
+      con.query(query, values, (err, result) => {
+        if (err) {
+          console.error('Error al insertar en la base de datos:', err);
+          res.status(500).json({ error: 'Error interno del servidor' });
+        } else {
+          console.log(`Práctica con ID ${id} insertada correctamente`);
+        }
+      });
+    });
+  
+    res.json({ message: 'Datos insertados correctamente' });
+  });
+
+app.delete("/practicas/:id", (req, res) => {
+    const idPractica = req.params.id;
+    con.query("DELETE FROM practicas WHERE id_practica = ?", [idPractica], function (error, results, field) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.send("La práctica se eliminó correctamente");
+        }
     });
 });
 
+app.get('/practicas', (req, res) => {
+    const query = 'SELECT * FROM practicas';
+  
+    con.query(query, (err, result) => {
+      if (err) {
+        console.error('Error al obtener las prácticas:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        res.json(result);
+      }
+    });
+  });
+  
+  // Ruta para obtener una práctica por id_practica
+  app.get('/practicas/:id_practica', (req, res) => {
+    const idPractica = req.params.id_practica;
+    const query = 'SELECT * FROM practicas WHERE id_practica = ?';
+  
+    con.query(query, [idPractica], (err, result) => {
+      if (err) {
+        console.error('Error al obtener la práctica por ID:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        if (result.length === 0) {
+          res.status(404).json({ error: 'Práctica no encontrada' });
+        } else {
+          res.json(result[0]);
+        }
+      }
+    });
+  });
+  
+  // Ruta para obtener una práctica por nombre_practica
+  app.get('/practicas/nombre/:nombre_practica', (req, res) => {
+    const nombrePractica = req.params.nombre_practica;
+    const query = 'SELECT * FROM practicas WHERE nombre_practica = ?';
+  
+    con.query(query, [nombrePractica], (err, result) => {
+      if (err) {
+        console.error('Error al obtener la práctica por nombre:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        if (result.length === 0) {
+          res.status(404).json({ error: 'Práctica no encontrada' });
+        } else {
+          res.json(result[0]);
+        }
+      }
+    });
+  });
 
-//correr tsc en terminal
-//node dist/api.js
+// Ruta para eliminar una práctica por id_practica
+app.delete('/practicas/:id_practica', (req, res) => {
+    const idPractica = req.params.id_practica;
+    const query = 'DELETE FROM practicas WHERE id_practica = ?';
+  
+    con.query(query, [idPractica], (err, result) => {
+      if (err) {
+        console.error('Error al eliminar la práctica por ID:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).json({ error: 'Práctica no encontrada' });
+        } else {
+          res.json({ message: 'Práctica eliminada correctamente' });
+        }
+      }
+    });
+  });
+  
+  // Ruta para eliminar una práctica por nombre_practica
+  app.delete('/practicas/nombre/:nombre_practica', (req, res) => {
+    const nombrePractica = req.params.nombre_practica;
+    const query = 'DELETE FROM practicas WHERE nombre_practica = ?';
+  
+    con.query(query, [nombrePractica], (err, result) => {
+      if (err) {
+        console.error('Error al eliminar la práctica por nombre:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).json({ error: 'Práctica no encontrada' });
+        } else {
+          res.json({ message: 'Práctica eliminada correctamente' });
+        }
+      }
+    });
+  });
+  
+  app.put('/practicas/:id_practica', (req, res) => {
+    const idPractica = req.params.id_practica;
+    const updatedData = req.body.updatedData;
+  
+    if (!updatedData) {
+      return res.status(400).json({ error: 'Datos de actualización no proporcionados' });
+    }
+  
+    const query = 'UPDATE practicas SET datos_practica = ? WHERE id_practica = ?';
+    const values = [JSON.stringify(updatedData), idPractica];
+  
+    con.query(query, values, (err, result) => {
+      if (err) {
+        console.error('Error al actualizar la práctica por ID:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).json({ error: 'Práctica no encontrada' });
+        } else {
+          res.json({ message: 'Práctica actualizada correctamente' });
+        }
+      }
+    });
+  });
+  
+  // Ruta para actualizar una práctica por nombre_practica
+  app.put('/practicas/nombre/:nombre_practica', (req, res) => {
+    const nombrePractica = req.params.nombre_practica;
+    const updatedData = req.body.updatedData;
+  
+    if (!updatedData) {
+      return res.status(400).json({ error: 'Datos de actualización no proporcionados' });
+    }
+  
+    const query = 'UPDATE practicas SET datos_practica = ? WHERE nombre_practica = ?';
+    const values = [JSON.stringify(updatedData), nombrePractica];
+  
+    con.query(query, values, (err, result) => {
+      if (err) {
+        console.error('Error al actualizar la práctica por nombre:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).json({ error: 'Práctica no encontrada' });
+        } else {
+          res.json({ message: 'Práctica actualizada correctamente' });
+        }
+      }
+    });
+  });
+  
+  
