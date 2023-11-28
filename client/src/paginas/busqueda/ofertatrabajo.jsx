20fd04824db8fb6748b3060ecb4a07c2e0254b21
Jose Urbina <joseurbina0208@gmail.com>
2023-11-28 18:17:07 -0300

    login

--- client/src/paginas/busqueda/ofertatrabajo.jsx
+++ client/src/paginas/busqueda/ofertatrabajo.jsx
@@ -1,46 +1,53 @@
 import React, { useEffect, useState } from "react";
 
 export const OfertaTrabajo = () => {
-  const [practicas, setPracticas] = useState([]); //se declaran las varibles practicas y set practicas practicas cumple la funcionalidad de almacenar objetos relacionados
-  //SetPracticas se utiliza para modificar el estado del objeto practica
+  const [practicas, setPracticas] = useState([]);
 
   useEffect(() => {
-    //Se utiliza la funcion fetch para cargar el archivo JSON
-    fetch("/testjsons/oferta1.json")
-      .then((datos) => {
-        //Se interpretan los datos como tipo JSON
-        return datos.json();
+    // Realizar la solicitud GET al servidor
+    fetch("http://localhost:3001/practicas")
+      .then((response) => {
+        // Verificar si la respuesta es exitosa (código 200)
+        if (!response.ok) {
+          throw new Error(`Error al obtener las prácticas: ${response.status}`);
+        }
+        // Interpretar la respuesta como JSON
+        return response.json();
       })
       .then((data) => {
-        //setPracticas va ir actualizando el valor del objeto practicas
-        setPracticas(data.practicas);
+        // Actualizar el estado con los datos obtenidos
+        setPracticas(data);
+      })
+      .catch((error) => {
+        console.error("Error en la solicitud GET:", error);
+        // Puedes manejar el error aquí, por ejemplo, mostrar un mensaje al usuario
       });
   }, []);
 
   return (
-    <div class="w-3/4 m-auto">
+    <div className="w-3/4 m-auto">
       {practicas.map((practica) => (
-        <div key={practica.id} class="py-6">
-          <div class="border border-black rounded-lg col-span-1 p-5 bg-gray-50 drop-shadow-lg grid grid-cols-6">
-            <h3 class="col-span-full">{practica.title}</h3>
-            <p class="col-span-2">{practica.nombreEmpresa}</p>
-            <p class="col-span-2">{practica.ubicacion}</p>
-            <div class="col-span-2 flex justify-end">
-              <div className= "drop-shadow-lg grid grid-cols w-3/4">
-                <img src={practica.imagen} alt="imagen de empresa" class="w-1/2"/>
+        <div key={practica.id_practica} className="py-6">
+          <div className="border border-black rounded-lg col-span-1 p-5 bg-gray-50 drop-shadow-lg grid grid-cols-6">
+            <h3 className="col-span-full">{practica.titulo}</h3>
+            <p className="col-span-2">{practica.nombre_empresa}</p>
+            <p className="col-span-2">{practica.ubicacion}</p>
+            <div className="col-span-2 flex justify-end">
+              <div className="drop-shadow-lg grid grid-cols w-3/4">
+                <img src={practica.imagen} alt="imagen de empresa" className="w-1/2" />
               </div>
             </div>
-            <div class="col-span-full flex justify-center">
-              <p>{practica.descripcion}</p> 
+            <div className="col-span-full flex justify-center">
+              <p>{practica.descripcion}</p>
             </div>
-            <div class="col-span-full flex justify-center">
-                <button
-                  type="button"
-                  class="border rounded-xl bg-blue-600 text-white p-2 px-6 shadow drop-shadow-xl mx-4"
-                >
-                  Postular
-                </button>
-              </div> 
+            <div className="col-span-full flex justify-center">
+              <button
+                type="button"
+                className="border rounded-xl bg-blue-600 text-white p-2 px-6 shadow drop-shadow-xl mx-4"
+              >
+                Postular
+              </button>
+            </div>
           </div>
         </div>
       ))}
--- client/src/paginas/iniciarsesion/iniciarsesion.jsx
+++ client/src/paginas/iniciarsesion/iniciarsesion.jsx
@@ -1,46 +1,62 @@
-import React from "react";
-import {useForm} from "react-hook-form"; //se carga el useForm de react-hook-form para el manejo de formularios 
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-export const IniciarSesion = () => { 
-  const {register,formState:{errors},handleSubmit} = useForm(); //la variable register es util para capturar y validar los datos del formulario
-  //formState accede al objeto errors que almacena los errores de validacion que ocurren en el campo del formulario
-  
-  const onSubmit = (data) => { 
-    console.log(data);
-  }
+export const IniciarSesion = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const [mensaje, setMensaje] = useState(null);
 
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch("http://localhost:3001/usuarios/verificarCredenciales", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+
+      if (result.autenticado) {
+        setMensaje("Inicio de sesión exitoso");
+      } else {
+        setMensaje("Credenciales incorrectas. Inténtalo de nuevo.");
+      }
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+      setMensaje("Error al iniciar sesión. Por favor, inténtalo de nuevo.");
+    }
+  };
 
   return (
     <div className="flex justify-center pt-16">
       <div className="w-1/2">
-        <form className="px-10 bg-gray-50 border rounded-md drop-shadow-lg"
-        onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className="px-10 bg-gray-50 border rounded-md drop-shadow-lg"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div>
             <h1 className="text-4xl font-bold text-center py-10">Iniciar sesión</h1>
           </div>
           <div className="grid grid-cols-8">
-              {
-                errors.email?.type === "required" && ( 
-                  <strong class="col-span-full flex justify-start p-2">* Ingrese un email valido</strong>
-                )
-              }
+            {errors.email?.type === "required" && ( 
+              <strong className="col-span-full flex justify-start p-2">* Ingrese un email válido</strong>
+            )}
             <div className="col-span-full flex justify-center p-2">
               <input
-                className=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
                 type="email"
                 id="email"
                 placeholder="Correo electrónico" 
                 {...register("email",{required:true})} 
               />
             </div>
-              {
-                errors.password?.type === "required" && (
-                  <strong class="col-span-full flex justify-start p-2">* Ingrese una contraseña valida</strong>
-                )
-              }
+            {errors.password?.type === "required" && (
+              <strong className="col-span-full flex justify-start p-2">* Ingrese una contraseña válida</strong>
+            )}
             <div className="col-span-full flex justify-center p-2">
               <input
-                className=" placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
+                className="placeholder-black border border-black rounded-md pl-2 w-full drop-shadow-lg py-1"
                 type="password"
                 id="password"
                 placeholder="Contraseña"
@@ -50,12 +66,17 @@ export const IniciarSesion = () => {
             <div className="col-span-full py-10 flex justify-center">
               <button
                 type="submit"
-                class="w-1/3 h-12 bg-blue-600 rounded-md text-white font-bold drop-shadow-lg border border-gray-400"
+                className="w-1/3 h-12 bg-blue-600 rounded-md text-white font-bold drop-shadow-lg border border-gray-400"
               >
                 Acceder
               </button>
             </div>
           </div>
+          {mensaje && (
+            <div className="col-span-full flex justify-center p-2">
+              <strong>{mensaje}</strong>
+            </div>
+          )}
         </form>
       </div>
     </div>
